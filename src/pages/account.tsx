a654ccbab5f476e82afb10dd882b146011607426
takanome_dev <takanomedev221@gmail.com>
2022-09-29 16:55:32 +0000

    feat: add account page

---
+++ src/pages/account.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AccountPage = () => (
+  <div className="flex items-center justify-center">
+    <p className="text-5xl">AccountPage</p>
+  </div>
+);
+
+export default AccountPage;

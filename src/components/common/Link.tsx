b8c8386ea237165927abe3d3b6c33d61d643326f
takanome_dev <takanomedev221@gmail.com>
2022-10-02 19:51:55 +0000

    feat: add password reset flow

--- src/components/common/Link.tsx
+++ src/components/common/Link.tsx
@@ -1,21 +1,26 @@
+/* eslint-disable react/require-default-props */
 import Link from 'next/link';
 import React from 'react';
+import { IconType } from 'react-icons/lib';
 
 interface LinkProps {
   path: string;
   title: string;
-  // eslint-disable-next-line react/require-default-props
   className?: string | undefined;
+  Icon?: IconType;
 }
 
 const LinkComponent = (props: LinkProps) => {
-  const { path, title, className } = props;
+  const { path, title, className, Icon } = props;
 
   const classes = `${className!} text-blue-500 text-2xl hover:underline`;
 
   return (
     <Link href={path}>
-      <a className={classes}>{title}</a>
+      <a className={classes}>
+        {Icon && <Icon size={18} className="mr-4 text-gray-700" />}
+        <span>{title}</span>
+      </a>
     </Link>
   );
 };
---
+++ src/pages/reset-password.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import NewPassword from '@components/PasswordReset/NewPassword';
+import ResetPassword from '@components/PasswordReset/ResetPassword';
+import ResetSuccessfully from '@components/PasswordReset/ResetSuccessfully';
+import VerifyEmail from '@components/PasswordReset/VerifyEmail';
+
+const ResetPasswordPage = () => <ResetSuccessfully />;
+
+export default ResetPasswordPage;

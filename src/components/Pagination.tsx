703309a4f2eaed8ea52288bcc7be5a90dd51a287
takanome_dev <takanomedev221@gmail.com>
2022-10-31 22:17:19 +0000

    refactor: get products count and display them

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,18 +1,59 @@
-import Link from 'next/link';
+import { gql, useQuery } from '@apollo/client';
 import React from 'react';
 
-export default function Pagination() {
+import Link from './common/Link';
+
+interface PaginationProps {
+  page: number;
+  pageSize: number;
+  // onPageCHange: (page: number) => void;
+}
+
+interface ProductsCountQuery {
+  productsCount: number;
+}
+
+const PRODUCTS_COUNT_QUERY = gql`
+  query {
+    productsCount
+  }
+`;
+
+export default function Pagination({ page, pageSize }: PaginationProps) {
+  const { data } = useQuery<ProductsCountQuery>(PRODUCTS_COUNT_QUERY);
+  const pages = Math.ceil(data!.productsCount / pageSize);
+  console.log({ pages, page });
+  // TODO: disable buttons
+
   return (
     <div className="w-[300px] grid grid-cols-[repeat(5,_auto)] rounded-2xl my-12 mx-auto overflow-hidden border border-gray-300">
-      <Link href="/#">
-        <a className="p-4 text-2xl border-r border-r-gray-300">⬅️ Prev</a>
-      </Link>
-      <p className="p-4 text-2xl border-r border-r-gray-300">1</p>
-      <p className="p-4 text-2xl border-r border-r-gray-300">..</p>
-      <p className="p-4 text-2xl border-r border-r-gray-300">10</p>
-      <Link href="/#">
-        <a className="p-4 text-2xl">➡️ Next</a>
-      </Link>
+      <Link
+        path={`/products?page=${page - 1}`}
+        className="p-4 text-2xl border-r border-r-gray-300"
+        title="⬅️ Prev"
+        disabled={page <= 1}
+      />
+      <Link
+        path={`/products?page=${page + 1}`}
+        className="p-4 text-2xl border-r border-r-gray-300"
+        title={`${page}`}
+      />
+      <Link
+        path={`/products?page=${page + 1}`}
+        className="p-4 text-2xl border-r border-r-gray-300"
+        title="..."
+      />
+      <Link
+        path={`/products?page=${pages}`}
+        className="p-4 text-2xl border-r border-r-gray-300"
+        title={`${pages}`}
+      />
+      <Link
+        path={`/products?page=${page + 1}`}
+        className="p-4 text-2xl"
+        title="➡️ Next"
+        disabled={page >= pages}
+      />
     </div>
   );
 }
--- src/components/Product/Products.tsx
+++ src/components/Product/Products.tsx
@@ -1,8 +1,8 @@
 import { gql, useQuery } from '@apollo/client';
-import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import React from 'react';
 
 import { Product } from '@components/types';
-import paginate from '@lib/paginate';
 
 import { CardList } from '../Card';
 import Pagination from '../Pagination';
@@ -12,8 +12,8 @@ interface ProductsQuery {
 }
 
 const ALL_PRODUCTS_QUERY = gql`
-  query ALL_PRODUCTS_QUERY {
-    products {
+  query ALL_PRODUCTS_QUERY($skip: Int!, $take: Int!) {
+    products(skip: $skip, take: $take) {
       id
       name
       description
@@ -31,19 +31,24 @@ const ALL_PRODUCTS_QUERY = gql`
 `;
 
 export default function Products() {
-  const PAGE_SIZE = 9;
-  const [currentPage, setCurrentPage] = useState(1);
-  const { data, error, loading } = useQuery<ProductsQuery>(ALL_PRODUCTS_QUERY);
+  const router = useRouter();
+  const PAGE_SIZE = 3;
+  const currentPage = router.query.page ?? 1;
+
+  const { data, error, loading } = useQuery<ProductsQuery>(ALL_PRODUCTS_QUERY, {
+    variables: {
+      skip: +currentPage * PAGE_SIZE - PAGE_SIZE,
+      take: PAGE_SIZE,
+    },
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
-  const products = paginate<Product>(data?.products, currentPage, PAGE_SIZE);
-
   return (
     <div className="pb-8">
-      <CardList products={products} />
-      <Pagination />
+      <CardList products={data?.products as Product[]} />
+      <Pagination page={+currentPage} pageSize={PAGE_SIZE} />
     </div>
   );
 }

48c73e43dd975bc1b665390cb537dd4e72d1675c
takanome_dev <takanomedev221@gmail.com>
2022-10-31 15:29:42 +0000

    feat: handle paginate data

--- src/components/Card/CardList.tsx
+++ src/components/Card/CardList.tsx
@@ -1,4 +1,3 @@
-import { gql, useQuery } from '@apollo/client';
 import React from 'react';
 
 import { Product } from '@components/types';
@@ -7,38 +6,10 @@ import { Product } from '@components/types';
 
 import Card from './Card';
 
-interface ProductsQuery {
-  products: Product[];
-}
-
-const ALL_PRODUCTS_QUERY = gql`
-  query ALL_PRODUCTS_QUERY {
-    products {
-      id
-      name
-      description
-      price
-      stock
-      status
-      photo {
-        id
-        image {
-          publicUrlTransformed
-        }
-      }
-    }
-  }
-`;
-
-export default function CardList() {
-  const { data, error, loading } = useQuery<ProductsQuery>(ALL_PRODUCTS_QUERY);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-
+export default function CardList({ products }: { products: Product[] }) {
   return (
     <div className="grid grid-cols-[repeat(auto-fill,_minmax(30rem,_1fr))] gap-8">
-      {data?.products.map((product) => (
+      {products.map((product) => (
         <Card key={product.id} product={product} />
       ))}
     </div>
--- src/components/Product/Products.tsx
+++ src/components/Product/Products.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+import React, { useState } from 'react';
+
+import { Product } from '@components/types';
+import paginate from '@lib/paginate';
 
 import { CardList } from '../Card';
 import Pagination from '../Pagination';
 
+interface ProductsQuery {
+  products: Product[];
+}
+
+const ALL_PRODUCTS_QUERY = gql`
+  query ALL_PRODUCTS_QUERY {
+    products {
+      id
+      name
+      description
+      price
+      stock
+      status
+      photo {
+        id
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
 export default function Products() {
+  const PAGE_SIZE = 9;
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data, error, loading } = useQuery<ProductsQuery>(ALL_PRODUCTS_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const products = paginate<Product>(data?.products, currentPage, PAGE_SIZE);
+
   return (
     <div className="pb-8">
-      <CardList />
+      <CardList products={products} />
       <Pagination />
     </div>
   );

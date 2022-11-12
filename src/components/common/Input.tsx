21a48081c53b5edf700641a3523ccd680317d6cf
takanome_dev <takanomedev221@gmail.com>
2022-11-12 12:21:30 +0000

    feat: add view password icon on input

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -4,7 +4,8 @@
 
 import { useField, useFormikContext } from 'formik';
 import { NextPage } from 'next';
-import React from 'react';
+import React, { useState } from 'react';
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
 
 import InputError from './InputError';
 
@@ -20,36 +21,72 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   type?: string;
   placeholder?: string;
   error?: string;
+  isPassword?: boolean;
 }
 
-const Input: NextPage<InputProps> = ({ label, error, ...props }) => {
+const Input: NextPage<InputProps> = ({
+  label,
+  error,
+  type,
+  isPassword = false,
+  ...props
+}) => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
   const [field, meta] = useField(props);
   const { values, setFieldValue } = useFormikContext();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let { type, name, value } = e.currentTarget;
+    let { type: inputType, name, value } = e.currentTarget;
 
-    if (type === 'file') {
+    if (inputType === 'file') {
       value = e.currentTarget.files?.[0] as any;
     }
 
     setFieldValue(name, value);
   };
 
+  const togglePasswordVisible = () => setPasswordVisible(!passwordVisible);
+
   const errorMessage = error || (meta.touched && meta.error);
 
   return (
     <div className="relative my-6">
-      <input
-        {...field}
-        {...props}
-        id={props.name}
-        className={`peer py-4 px-6 placeholder-transparent rounded-xl text-2xl border-2 ${
+      <div
+        className={`flex items-center justify-between border-2 ${
           errorMessage ? 'border-red-500' : 'border-gray-300'
-        } outline-none text-gray-700 w-full transition-all duration-[300ms] ease-in-out focus:delay-100 focus:border-cyan focus:ring-2 focus:ring-cyan file:border-none file:p-4 file:rounded-lg file:font-semibold file:mr-5 file:cursor-pointer file:bg-gradient-to-r file:from-cyan file:to-teal hover:file:opacity-80 file:text-gray-700`}
-        value={props.name !== 'image' ? (values as any)[props.name] : undefined}
-        onChange={handleChange}
-      />
+        } rounded-xl`}
+      >
+        <input
+          {...field}
+          {...props}
+          id={props.name}
+          type={isPassword ? (passwordVisible ? 'text' : 'password') : type}
+          className="peer py-4 px-6 placeholder-transparent text-2xl outline-none text-gray-700 w-full transition-all duration-[300ms] ease-in-out focus:delay-100 focus:border-cyan focus:ring-2 focus:ring-cyan file:border-none file:p-4 file:rounded-lg file:font-semibold file:mr-5 file:cursor-pointer file:bg-gradient-to-r file:from-cyan file:to-teal hover:file:opacity-80 file:text-gray-700"
+          value={
+            props.name !== 'image' ? (values as any)[props.name] : undefined
+          }
+          onChange={handleChange}
+        />
+        {isPassword ? (
+          passwordVisible ? (
+            <button
+              type="button"
+              className="p-3 rounded-full hover:bg-gray-100"
+              onClick={togglePasswordVisible}
+            >
+              <AiOutlineEye size={20} />
+            </button>
+          ) : (
+            <button
+              type="button"
+              className="p-3 rounded-full hover:bg-gray-100"
+              onClick={togglePasswordVisible}
+            >
+              <AiOutlineEyeInvisible size={20} />
+            </button>
+          )
+        ) : null}
+      </div>
       <label
         className={`text-xl ${
           errorMessage ? 'text-red-500' : 'text-gray-600'
